867b8ce54752dbfc812080e50da4b46f2ed43447
ashwinpanchal <ashwinpanchal.iitb@gmail.com>
2024-03-26 15:01:33 +0530

    Added Routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,15 @@ import bodyParser from "body-parser";
 
 import { PORT } from "./config/serverConfig.js";
 import { dbConnect } from "./config/dbConfig.js";
+import { apiRouter } from "./routes/index.js";
 
 const startAndSetup = async () => {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRouter);
 
   app.listen(PORT, async () => {
     console.log("Server running on port", PORT);
